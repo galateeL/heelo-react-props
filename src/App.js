caa1f1ceadadd762f7fb2fc897617756464e6cfe
Galatée Lardière <galatee.lardiere@gmail.com>
2022-04-03 11:18:58 +0200

    Exemple de passage de valeurs lors de l'appel du composant

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import Avatar from './components/Avatar';
 import './style.css';
 
-const bart = {
-  avatar : "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
-  firstName : 'Bart',
-  lastName : 'Simpson'
-};
-
 function App() {
-// utilisation d'un spread operator pour passer toutes les propriétés de l'objet "bart" comme props distinctes au composant Avatar
+// passage de valeurs aux propriétés lors de l'appel du composant Avatar
   return (
     <div>
       <h1 className="text-center">Simpsons App 😎</h1>
       <div>
-          <Avatar {...bart}/>
+          <Avatar avatar="https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png" firstName="Bart" lastName="Simpson" />
           <Avatar avatar="https://www.stickees.com/files/cartoon/the-simpsons/2248-homer-simpson-happy.png" firstName="Homer" lastName="Simpson" />
           <Avatar avatar="https://www.stickees.com/files/cartoon/the-simpsons/2250-lisa-simpson-sticker.png" firstName='Lisa' lastName='Simpson'/>
       </div>
